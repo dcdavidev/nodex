15d558fe9975f37d2302af98b6a9c00c56d2d974
Davide Di Criscito <davidedc@outlook.com>
2025-10-31 12:48:41 +0100

    Add multiple exports to index.ts

--- packages/nodex/src/index.ts
+++ packages/nodex/src/index.ts
@@ -1,1 +1,24 @@
+// Main exports
 export { Nodex } from './nodex.js';
+
+// Error exports
+export { BaseError, ConfigError, ValidationError } from './errors/index.js';
+
+// Helper exports
+export { getNodeEnv, isDevelopment } from './helpers/index.js';
+
+// Schema exports
+export { ErrorMetadata, NodeEnv, NodexConfig, Port } from './schemas/index.js';
+
+// Constants exports
+export {
+  DEFAULT_PORT,
+  MIN_PORT,
+  MAX_PORT,
+  NODE_ENV_VALUES,
+  LOG_LEVELS,
+  ERROR_CODES,
+} from './consts.js';
+
+// Logger exports
+export { Logger } from './logger.js';
