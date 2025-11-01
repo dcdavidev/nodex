64dc80264fef1935ecbec92a36ac3396556dafac
Davide Di Criscito <davidedc@outlook.com>
2025-11-01 14:04:32 +0100

    fix: update imports

--- packages/nodex/src/logger.ts
+++ packages/nodex/src/logger.ts
@@ -2,7 +2,7 @@ import pretty from 'pino-pretty';
 
 import pino from 'pino';
 
-import { LOG_LEVELS } from './consts.js';
+import { LOG_LEVELS } from './consts/log-levels.js';
 import { isDevelopment } from './helpers/is-development.js';
 
 const prettyStream = pretty({
