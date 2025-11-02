d45e89fa1d23d5d1bb981685b4c861866b030f06
Davide Di Criscito <davidedc@outlook.com>
2025-11-02 15:40:40 +0100

    feat(nodex): prefer custom schemas from official types

--- packages/nodex/src/schemas/cookie-parser.ts
+++ packages/nodex/src/schemas/cookie-parser.ts
@@ -1,22 +1,9 @@
+import type { CookieParseOptions } from 'cookie-parser';
+
 import { z } from 'zod';
 
 export const CookieParserOptionsSchema = z
-  .union([
-    z.boolean(),
-    z.object({
-      secret: z.union([z.string(), z.array(z.string())]).optional(),
-      options: z
-        .object({
-          decode: z
-            .function({
-              input: [z.string()],
-              output: z.string(),
-            })
-            .optional(),
-        })
-        .optional(),
-    }),
-  ])
+  .union([z.boolean(), z.custom<CookieParseOptions>()])
   .optional()
   .default(true);
 
--- packages/nodex/src/schemas/cors.ts
+++ packages/nodex/src/schemas/cors.ts
@@ -1,36 +1,11 @@
+import type { CorsOptions } from 'cors';
+
 import { z } from 'zod';
 
-import {
-  DEFAULT_CORS_ALLOWED_HEADERS,
-  DEFAULT_CORS_CREDENTIALS,
-  DEFAULT_CORS_METHODS,
-  DEFAULT_CORS_OPTIONS,
-  DEFAULT_CORS_OPTIONS_SUCCESS_STATUS,
-  DEFAULT_CORS_ORIGINS,
-} from '../consts/cors.js';
+import { DEFAULT_CORS_OPTIONS } from '../consts/cors.js';
 
 export const CorsOptionsSchema = z
-  .union([
-    z.boolean(),
-    z.object({
-      enabled: z.boolean().default(true),
-      origin: z.array(z.url()).optional().default(DEFAULT_CORS_ORIGINS),
-      methods: z
-        .array(z.enum(DEFAULT_CORS_METHODS))
-        .optional()
-        .default([...DEFAULT_CORS_METHODS]),
-      allowedHeaders: z
-        .array(z.string())
-        .optional()
-        .default(DEFAULT_CORS_ALLOWED_HEADERS),
-      credentials: z.boolean().optional().default(DEFAULT_CORS_CREDENTIALS),
-      optionsSuccessStatus: z
-        .number()
-        .int()
-        .optional()
-        .default(DEFAULT_CORS_OPTIONS_SUCCESS_STATUS),
-    }),
-  ])
+  .union([z.boolean(), z.custom<CorsOptions>()])
   .optional()
   .default(DEFAULT_CORS_OPTIONS);
 
